4b24c38cd19161c1e9ecd82659101c37ae3cb25f
hyagosouzza <hyagosouzza@hotmail.com>
2016-10-19 12:08:26 -0200

    Update package-info.java

--- Cpf/src/main/java/com/github/hyagosouzza/cs20162aula06/cpf/package-info.java
+++ Cpf/src/main/java/com/github/hyagosouzza/cs20162aula06/cpf/package-info.java
@@ -1,6 +1,7 @@
 /**
  * Pacote : com.github.hyagosouzza.cs20162aula06.cpf.
- * Pacote que contém os métodos para verificar a validade de um cpf, e qual desses métodos é o mais rápido.
+ * Pacote que contém os métodos para verificar a validade de um cpf,
+ * e qual desses métodos é o mais rápido.
  * @version 1.1
  */
 package com.github.hyagosouzza.cs20162aula06.cpf;
