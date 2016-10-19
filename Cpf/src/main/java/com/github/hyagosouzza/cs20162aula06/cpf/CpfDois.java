5c108df1fd4bbc776c32243f78aa0ddf20757400
hyagosouzza <hyagosouzza@hotmail.com>
2016-10-19 15:12:15 -0200

    Update CpfDois.java

--- Cpf/src/main/java/com/github/hyagosouzza/cs20162aula06/cpf/CpfDois.java
+++ Cpf/src/main/java/com/github/hyagosouzza/cs20162aula06/cpf/CpfDois.java
@@ -9,8 +9,8 @@ package com.github.hyagosouzza.cs20162aula06.cpf;
  * Classe para verificar a validade de um cpf.
  * @version 1.1
  */
-public class CpfDois {
-    
+public class CpfDois
+{
     /**
      * Método para retornar um boleano sobre a validade de um cpf.
      * @param d final int[] - vetor com 11 posições
